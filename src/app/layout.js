b1ce386b7aecc3404e55dcde88f50921dd228139
Faizuddinq <faizqureshi6126@gmail.com>
2025-01-13 16:06:13 +0530

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Recharts Next.js',
+  title: 'Recharts Mini App',
   description: 'An example of using Recharts with Next.js',
 };
 
